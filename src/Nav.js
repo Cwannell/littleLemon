8cfc04a2e6cc3dbd1f02469b6d71517983f6abd9
Your Name <callumwannell@gmail.com>
2026-01-07 16:46:02 +0900

    jan 7 second update

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
 import Logo from './Images/Logo.svg'
 
 const Nav = () => {
+ 
+    const [menuOpen, SetMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        SetMenuOpen(!menuOpen);
+    }
+
+
 return (
-    <nav className='navbar'>
-        <a className='logo' href='/'><img src={Logo}/></a>
-        <ul className='nav-list'>
-            <li className='nav-item'><a href="">Home</a></li>
-            <li className='nav-item'><a href="">About</a></li>
-            <li className='nav-item'><a href="">Menu</a></li>
-            <li className='nav-item'><a href="">Reservations</a></li>
-            <li className='nav-item'><a href="">Order Online</a></li>
-            <li className='nav-item'><a href="">Login</a></li>
+    <nav className={`navbar ${menuOpen ? 'open' : ""} `}>
+        <a className='logo' href='/'>
+        <img src={Logo} alt='Logo'/>
+        </a>
+
+        {/* nav-icon */}
+
+        <div className='nav-icon' onClick={toggleMenu}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            
+        </div>
+
+       <ul className={`nav-list ${menuOpen ? 'visible' : ""}`}>
+            <li className='nav-item'><a href="/">Home</a></li>
+            <li className='nav-item'><a href="/">About</a></li>
+            <li className='nav-item'><a href="/">Menu</a></li>
+            <li className='nav-item'><a href="/">Reservations</a></li>
+            <li className='nav-item'><a href="/">Order Online</a></li>
+            <li className='nav-item'><a href="/">Login</a></li>
             
         </ul>
     </nav>
